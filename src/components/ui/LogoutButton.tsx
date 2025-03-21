2d195738c84165ce9c1c9c4b39d07e87aec72a13
Paulo Suljic <paulo.suljic@gmail.com>
2025-03-21 03:42:42 +0100

    LogoutButtonRemoved

--- src/components/ui/LogoutButton.tsx
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "../../providers/AuthProvider";
-
-const LogoutButton = () => {
-  const { logout } = useAuth();
-
-  return <button onClick={logout}>Logout</button>;
-};
-
-export default LogoutButton;
