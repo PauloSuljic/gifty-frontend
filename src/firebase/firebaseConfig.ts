470a73a0221785c9d57a9f5b0367f93e91f165d2
Paulo Suljic <paulo.suljic@gmail.com>
2025-03-26 02:12:42 +0100

    test prod

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,4 +1,5 @@
 console.log("🔥 Firebase API Key:", import.meta.env.VITE_FIREBASE_API_KEY);
+console.log("🔍 Full Env:", import.meta.env);
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
